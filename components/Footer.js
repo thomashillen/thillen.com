27398373e35c79ca6073c5b6ead533a4a7e88a9c
thomashillen <47916202+thomashillen@users.noreply.github.com>
2023-04-18 18:56:00 -0400

    reset

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-800 dark:bg-gray-900 text-white text-center p-4">
-      &copy; {new Date().getFullYear()} Thomas Hillenmeyer. All rights reserved.
-    </footer>
-  );
-};
-
-export default Footer;
