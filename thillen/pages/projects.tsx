43319672d76103f8d9eee0f2702ecbf278910307
thomashillen <47916202+thomashillen@users.noreply.github.com>
2023-11-28 04:10:22 -0500

    Refactor projects page layout and add image components

--- thillen/pages/projects.tsx
+++ thillen/pages/projects.tsx
@@ -1,9 +1,13 @@
-import Head from "next/head"
-import Link from "next/link"
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
-import { Icons } from "@/components/icons"
-import { Layout } from "@/components/layout"
-import { Card, CardContent, CardHeader } from "@/components/ui/card"
+
+
+import { Icons } from "@/components/icons";
+import { Layout } from "@/components/layout";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
 
 export default function ProjectsPage() {
   const projects = [
@@ -11,20 +15,20 @@ export default function ProjectsPage() {
       title: "Burger Builder",
       description: "A fun and interactive way to build your perfect burger.",
       link: "https://my-perfect-burger.vercel.app/",
-      imageUrl: "/path-to-burger-builder-image.jpg", // Replace with your image URL
+      imageUrl: "/BurgerBuilderScreenshot.png",
     },
-        {
+    {
       title: "nannü website",
       description: "A website for a fashion brand.",
       link: "https://nannu.us/",
+      imageUrl: "/nannuScreenshot.png",
     },
     {
       title: "My Portfolio",
       description: "This website!",
       link: "https://thillen.com",
-      imageUrl: "/path-to-portfolio-image.jpg", // Replace with your image URL
+      imageUrl: "/thillenScreenshot.png",
     },
-
 
     // ... more projects
   ]
@@ -36,12 +40,12 @@ export default function ProjectsPage() {
         {/* ... other head elements ... */}
       </Head>
       <section className="container mx-auto p-6">
-        <h1 className="text-3xl font-extrabold mb-4">Projects</h1>
+        <h1 className="mb-4 text-3xl font-extrabold">Projects</h1>
         <p className="mb-6">
           Here are some of my projects. I am always working on new things so
           check back soon!
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           {projects.map((project, index) => (
             <Link
               key={index}
@@ -50,7 +54,7 @@ export default function ProjectsPage() {
               rel="noopener noreferrer"
             >
               <div className="block h-full">
-                <Card className="hover:bg-secondary transition-colors h-full">
+                <Card className="hover:bg-secondary h-full transition-colors">
                   <CardHeader>
                     <div className="flex items-center justify-between">
                       <h2>{project.title}</h2>
@@ -58,12 +62,19 @@ export default function ProjectsPage() {
                     </div>
                   </CardHeader>
                   <div className="p-4">
-                    <img
+                    {/* <img
                       src={project.imageUrl}
                       alt={project.title}
                       className="mb-4 w-full object-cover"
-                    />{" "}
+                    />{" "} */}
                     {/* Image */}
+                    <Image
+                      src={project.imageUrl}
+                      alt={project.title}
+                      width={500} // replace with your image's width
+                      height={300} // replace with your image's height
+                      className="mb-4 object-cover"
+                    />
                     <CardContent>
                       <p>{project.description}</p>
                     </CardContent>
